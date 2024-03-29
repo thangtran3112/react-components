8ac25f5a482c0a7da1475936fdc365d2d5f4a764
Thang Tran <trathanl@amazon.com>
2024-03-29 00:54:25 -0700

    Tree auto checking

--- tree-view/src/v3_search/TreeContext.js
+++ tree-view/src/v3_search/TreeContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer, useState } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 
 const TreeStateContext = createContext();
 
@@ -20,14 +20,75 @@ const updateAllNodes = (nodes, isExpanded) => {
       return {
         ...node,
         isExpanded,
-        children: updateAllNodes(node.children, isExpanded)
+        children: updateAllNodes(node.children, isExpanded),
       };
     }
     return { ...node, isExpanded };
   });
 };
 
-const searchNodesAndUpdateHighlight = (nodes, query) => {
+const collectIdsFromNode = (node, targetId, ids) => {
+  if (node.id === targetId) {
+    //if this node have the corresponding it, we will add all child id to the ids array
+    ids.add(targetId);
+    if (node.children) {
+      node.children.forEach((child) =>
+        collectIdsFromNode(child, child.id, ids)
+      );
+    }
+  } else {
+    //if this node is not having the corresponding id, try to find the id in its children
+    if (node.children) {
+      node.children.forEach((child) =>
+        collectIdsFromNode(child, targetId, ids)
+      );
+    }
+  }
+};
+
+const markAllNodes = (nodes, isChecked, ids) => {
+  return nodes.map((node) => {
+    const matching = ids.has(`${node.id}`);
+    console.log(`Current node.id = ${node.id} and matching=${matching}`);
+    //having children
+    if (node.children) {
+      if (matching) {
+        console.log(`Marking isChecked=${isChecked} for node at id=${node.id}`);
+        return {
+          ...node,
+          isChecked,
+          children: markAllNodes(node.children, isChecked, ids),
+        };
+      } else {
+        return {
+          ...node,
+          children: markAllNodes(node.children, isChecked, ids),
+        };
+      }
+    }
+
+    //no children
+    if (matching) {
+      console.log(`Marking isChecked=${isChecked} for node at id=${node.id}`);
+      return { ...node, isChecked };
+    } else {
+      return node;
+    }
+  });
+};
+
+const checkNodes = (nodes, checked, id) => {
+  console.log(`Dispatch check nodes with id=${id}, checked=${checked}`);
+  const ids = new Set();
+  nodes.forEach((node) => {
+    collectIdsFromNode(node, id, ids);
+  });
+  console.log(`Marking isChecked=${checked} for all nodes with: `, ids);
+
+  return markAllNodes(nodes, checked, ids);
+};
+
+const searchNodes = (nodes, query) => {
   nodes.forEach((node) => {
     let shouldHighlight = query.length
       ? node.name.toLowerCase().includes(query.toLowerCase())
@@ -36,7 +97,23 @@ const searchNodesAndUpdateHighlight = (nodes, query) => {
     node.isHighlight = shouldHighlight;
 
     if (node.children) {
-      searchNodesAndUpdateHighlight(node.children, query);
+      searchNodes(node.children, query);
+    }
+  });
+
+  return nodes;
+};
+
+const searchNodesHighlightPath = (nodes, query) => {
+  nodes.forEach((node) => {
+    let shouldHighlight = query.length
+      ? node.name.toLowerCase().includes(query.toLowerCase())
+      : false;
+
+    node.isHighlight = shouldHighlight;
+
+    if (node.children) {
+      searchNodesHighlightPath(node.children, query);
       if (node.children.some((child) => child.isHighlight)) {
         node.isHighlight = true;
       }
@@ -47,18 +124,22 @@ const searchNodesAndUpdateHighlight = (nodes, query) => {
 };
 
 const treeReducer = (state, action) => {
-  console.log("ttt", action);
+  //console.log("ttt", action);
   switch (action.type) {
     case "INIT_DATA":
       return action.data;
     case "TOGGLE_NODE":
       return toggleNode(state, action.id, action.isExpanded);
+    case "CHECK_NODE":
+      return checkNodes(state, action.isChecked, action.id);
     case "EXPAND_ALL":
       return updateAllNodes(state, true);
     case "COLLAPSE_ALL":
       return updateAllNodes(state, false);
     case "SEARCH":
-      return searchNodesAndUpdateHighlight(state, action.query);
+      return searchNodes(state, action.query);
+    case "SEARCH_HIGHLIGHT_SUBTREE":
+      return searchNodesHighlightPath(state, action.query);
     default:
       return state;
   }
